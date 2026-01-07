53b5165608cd0254aad1d30ab036f75b97c5c0aa
shakurah <emmanueladjerry@gmail.com>
2026-01-07 09:33:50 +0100

    Make width and height dynamic according to grid size

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const container = document.querySelector(".container");
+const CONTAINERSIZE = 500;
+let grid = 16; 
 
-for (let i = 0; i < 256; i++){
-    const div = document.createElement("div");
-    div.classList.add("grid-item");
-    container.appendChild(div);
+function isValidNum(num){
+    
 }
+function makeGrid(size){
+    container.innerHTML = "";
+    let totalSquares = size * size;
+    let squaresize = CONTAINERSIZE / size
 
-const gridItems =  document.querySelectorAll(".grid-item");
+    for (let i = 0; i < totalSquares; i++){
+        const div = document.createElement("div");
+        div.classList.add("grid-item")
 
-gridItems.forEach( gridItem => {
-    gridItem.addEventListener("mouseenter", () => {
-        gridItem.classList.add("pixelated");
-    })
+        div.style.width = `${squaresize}px`
+        div.style.height = `${squaresize}px`
+        container.appendChild(div);
+    }
+}
+makeGrid(grid)
+const btn =  document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+    let squareSide = Number(prompt("How many squares per side"));
+
+    makeGrid(squareSide);
+})
+
+
+container.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("grid-item")) {
+        e.target.classList.add("pixelated");
+    }
 })
